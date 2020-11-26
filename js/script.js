dc92c8bc785d295e5fac978c0aa1605c37c2ec47
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2020-11-26 01:06:50 +0100

    main content prepared for work

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var slider = document.getElementById("formControlRange");
+var output = document.getElementById("timeInput");
+output.value = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.value = this.value;
+}
+
+
+// Listen for input event on numInput.
+output.onkeydown = function(e) {
+    if(!((e.keyCode > 95 && e.keyCode < 106)
+      || (e.keyCode > 47 && e.keyCode < 58) 
+      || e.keyCode == 8)) {
+        return false;
+    }
+}
