b6516ec9e2cb2ff4e060fb0e5d4069a343b54344
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2020-11-27 21:17:57 +0100

    script: contact form functions and behavior

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+// ----------- Navbar Events ----------- //
+// ----- contact
+const contactForm = document.getElementById('ghostwriter-contact');
+let contactElements = contactForm.elements;   // 0=name , 1=email , 2=message , 3=submit , 4=close
+
+$("#name,#email,#message").on("change keyup paste", function(){
+
+  if(!contactElements[0].value == "" && !contactElements[1].value == "" && contactElements[1].value.includes('@'&&'.')  && !contactElements[2].value == ""){
+    contactElements[3].disabled = false;
+  }
+  else{
+    contactElements[3].disabled = true;
+  };
+
+});
+
+$(contactElements[4]).click(function() {
+  setTimeout(`contactForm.reset(); $('#submit-button').html('Send');`,300);
+});
+
+$("#submit-button").click(function(){
+  $("#submit-button").html('<i class="fas fa-heart"></i>');
+  setTimeout(`$('#contact-modal').modal('hide');`,1000);
+})
+// -----
+
+
+
+// ------------- User Input ------------ //
 var slider = document.getElementById("formControlRange");
 var output = document.getElementById("timeInput");
 output.value = slider.value; // Display the default slider value
