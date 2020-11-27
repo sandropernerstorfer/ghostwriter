1b4a3738dfdb1eac784f6fd3ff7f53e180e710b1
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2020-11-28 00:38:06 +0100

    theme switch dev started + styling adjusted

--- js/script.js
+++ js/script.js
@@ -22,12 +22,41 @@ $("#submit-button").click(function(){
   $("#submit-button").html('<i class="fas fa-heart"></i>');
   setTimeout(`$('#contact-modal').modal('hide');`,1000);
 })
-// -----
+
+// ----- light / dark mode
+
+const darkBtn = document.getElementById('dark-btn');
+darkBtn.addEventListener('click',function(){renderColor()},false);
+
+const localTheme = localStorage.getItem('ghostwriterTheme');
+
+// if(localTheme === null || localTheme === 'light'){
+//   localStorage.setItem('ghostwriterTheme','light');
+//   renderColor("light");
+// }
+// else if(localTheme === 'dark'){
+//   renderColor("dark");
+// }
+
+function renderColor(x){
+  $('body,header,footer,.footer-box-2,.tabs-section').css('background-color','#242425');
+  $('.footer-box-1,main,.working-section,.box-3,.box-4').css('background-color','#2E2E2E');
+  $('main,.tabs-section,.footer-box-1').css('box-shadow','none');
+  $('main').css({'border-top-left-radius':'20px','border-top-right-radius':'20px'})
+  $('.box-1,.box-2').css({'background-color':'#272727','box-shadow':'none'});
+  $('#timeInput,#input-text,.output-window,#name,#email').css({'background-color':'#272727','color':'#ccc','border-color':'#FFC900'});
+  $('.form-badge').css({'background-color':'#FFC900','color':'#000'});
+  $('.code-badge').css({'background-color':'#242425','color':'#ccc'});
+  $('.pre-html,.pre-js').css('color','#fff');
+  $('.modal-content').css('background-color','#272727');
+  $('.modal-header').css('background-color','#272727');
+  $('#contact-text').css('color','#ccc');
+}
 
 
 
 // ------------- User Input ------------ //
-var slider = document.getElementById("formControlRange");
+var slider = document.getElementById("input-range");
 var output = document.getElementById("timeInput");
 output.value = slider.value; // Display the default slider value
 
