afeaacf686f1d335ab5b507647bc04e997d64635
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2020-11-29 22:50:06 +0100

    theme switch event added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-// ----------- Navbar Events ----------- //
-// ----- contact
+// ----------------------------------------- Navbar Events ----------- //
+
+// ----- contact ----- //
+
 const contactForm = document.getElementById('ghostwriter-contact');
 let contactElements = contactForm.elements;   // 0=name , 1=email , 2=message , 3=submit , 4=close
 
@@ -23,7 +25,7 @@ $("#submit-button").click(function(){
   setTimeout(`$('#contact-modal').modal('hide');`,1000);
 })
 
-// ----- light / dark mode
+// ------ light / dark mode ------ //
 
 const lightBtn = document.getElementById('light-btn');
 const darkBtn = document.getElementById('dark-btn');
@@ -33,23 +35,90 @@ darkBtn.addEventListener('click',function(){renderColor("dark");localStorage.set
 
 if(localTheme === null || localTheme === 'light'){
   localStorage.setItem('ghostwriterTheme','light');
-  renderColor("light");
+  document.body.setAttribute('data-theme', 'light');
 }
 else if(localTheme === 'dark'){
-  renderColor("dark");
+  document.body.setAttribute('data-theme', 'dark');
 }
 
 function renderColor(x){
 
   if(x === "dark"){
-    document.body.setAttribute('data-theme', 'dark');
+    $('#theme-page').css({'opacity':'1','background-color':'#242425','z-index':'200','width':'100vw'});
+    $('#theme-icon').css({'opacity':'1','color':'#fff'}).removeClass('fa-sun').addClass('fa-moon');
+    setTimeout(function(){
+      document.body.setAttribute('data-theme', 'dark');
+    },750);
+    setTimeout(function(){
+      $('#theme-page').css({'opacity':'1','z-index':'200','width':'0vw'});
+      $('#theme-icon').css({'opacity':'0'});
+    },1000);
   }
   else if(x === "light"){
-    document.body.setAttribute('data-theme', 'light');
+
+    $('#theme-page').css({'opacity':'1','background-color':'#fff','z-index':'200','width':'100vw'});
+    $('#theme-icon').css({'opacity':'1'});
+    $('#theme-icon').css({'opacity':'1','color':'coral'}).removeClass('fa-moon').addClass('fa-sun');
+    setTimeout(function(){
+      document.body.setAttribute('data-theme', 'light');
+    },750);
+    setTimeout(function(){
+      $('#theme-page').css({'opacity':'1','background-color':'#fff','z-index':'200','width':'0vw'});
+      $('#theme-icon').css({'opacity':'0'});
+    },1000);
   }
 
 }
 
+// ---------------------------- Code Copy Functionality ---- //
+
+document.getElementById('copy-html').addEventListener('click', function(){collectToCopy('code-html');if(tooltipReady){showTooltip('tooltip-html');}});
+document.getElementById('copy-js').addEventListener('click', function(){collectToCopy('code-js');if(tooltipReady){showTooltip('tooltip-js');}});
+document.getElementById('copy-both').addEventListener('click', function(){collectToCopy('code-html','code-js');if(tooltipReady){showTooltip('tooltip-html','tooltip-js');}});
+
+function collectToCopy(a,b){
+  if(b==undefined){
+    const singleCopy = document.getElementById(a).innerText;
+    copyToClipboard(singleCopy);
+  }
+  else{
+    const firstText = document.getElementById(a).innerText;
+    const secondText = document.getElementById(b).innerText;
+    const combinedText = firstText.concat('\n','\n',secondText);
+    copyToClipboard(combinedText);
+  }
+}
+
+function copyToClipboard(i){
+  let tempTextarea = document.getElementById('copy-textarea');
+  tempTextarea.innerHTML = i;
+  tempTextarea.select();
+  document.execCommand('copy');
+}
+
+// ---------------------------------- Code Copy Popup ----- //
+
+let tooltipReady = true;
+
+function showTooltip(a,b){
+  tooltipReady = false;
+  if(b==undefined){
+    $('#'+a).toggleClass("toggle-hide toggle-show");
+    setTimeout(function(){
+      $('#'+a).toggleClass("toggle-hide toggle-show");
+      tooltipReady = true;
+    },600);
+  }
+  else{
+    $('#'+a).toggleClass("toggle-hide toggle-show");
+    $('#'+b).toggleClass("toggle-hide toggle-show");
+    setTimeout(function(){
+      $('#'+a).toggleClass("toggle-hide toggle-show");
+      $('#'+b).toggleClass("toggle-hide toggle-show");
+      tooltipReady = true;
+    },600);
+  }
+}
 
 
 // ------------- User Input ------------ //
@@ -76,56 +145,4 @@ output.onkeydown = function(e) {
 output.oninput = function() {
   slider.value = this.value;
   if(this.value==''){slider.value = 0};
-}
-
-
-
-// ----------------- Code Copy Functionality ---- //
-
-document.getElementById('copy-html').addEventListener('click', function(){collectToCopy('code-html');if(tooltipReady){showTooltip('tooltip-html');}});
-document.getElementById('copy-js').addEventListener('click', function(){collectToCopy('code-js');if(tooltipReady){showTooltip('tooltip-js');}});
-document.getElementById('copy-both').addEventListener('click', function(){collectToCopy('code-html','code-js');if(tooltipReady){showTooltip('tooltip-html','tooltip-js');}});
-
-function collectToCopy(a,b){
-  if(b==undefined){
-    const singleCopy = document.getElementById(a).innerText;
-    copyToClipboard(singleCopy);
-  }
-  else{
-    const firstText = document.getElementById(a).innerText;
-    const secondText = document.getElementById(b).innerText;
-    const combinedText = firstText.concat('\n','\n',secondText);
-    copyToClipboard(combinedText);
-  }
-}
-
-function copyToClipboard(i){
-  let tempTextarea = document.getElementById('copy-textarea');
-  tempTextarea.innerHTML = i;
-  tempTextarea.select();
-  document.execCommand('copy');
-}
-
-// -------Tooltip Popup --- //
-
-let tooltipReady = true;
-
-function showTooltip(a,b){
-  tooltipReady = false;
-  if(b==undefined){
-    $('#'+a).toggleClass("toggle-hide toggle-show");
-    setTimeout(function(){
-      $('#'+a).toggleClass("toggle-hide toggle-show");
-      tooltipReady = true;
-    },600);
-  }
-  else{
-    $('#'+a).toggleClass("toggle-hide toggle-show");
-    $('#'+b).toggleClass("toggle-hide toggle-show");
-    setTimeout(function(){
-      $('#'+a).toggleClass("toggle-hide toggle-show");
-      $('#'+b).toggleClass("toggle-hide toggle-show");
-      tooltipReady = true;
-    },600);
-  }
 }
